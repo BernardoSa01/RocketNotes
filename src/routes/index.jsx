020e7931a8a755d340cb3a46166aabd608521f86
bernardo <bernardo_nf@hotmail.com>
2024-10-23 10:11:51 -0300

    feat: update user access routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,22 @@
 import { BrowserRouter } from "react-router-dom";
 
+// importando o hook de autenticação, para acessar o usuário
+import { useAuth } from '../hooks/auth'
+
+
 import { AppRoutes } from "./app.routes";
 import { AuthRoutes } from './auth.routes'
 
 export function Routes() {
+  const { user } = useAuth()
+
   return(
+    /* Criando condicional:
+       Se o usuário estiver logado, renderize o 'AppRoutes'.
+       Senão, renderize o 'AuthRoutes'. 
+    */  
     <BrowserRouter>
-      <AuthRoutes />
+      {user ? <AppRoutes /> : <AuthRoutes />}
     </BrowserRouter>
   )
 }
