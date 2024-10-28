bcee1acef520cbdf5a2142263afe0491eaa6d9dc
bernardo <bernardo_nf@hotmail.com>
2024-10-28 11:12:50 -0300

    feat: add function to update user profile

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,21 +1,27 @@
 import { RiShutDownLine } from 'react-icons/ri';
 import { useAuth } from '../../hooks/auth'
+import { api } from '../../services/api'
+
+
 import { Container, Profile, Logout } from "./styles";
 
 export function Header() {
-  const { signOut } = useAuth()
+  const { signOut, user } = useAuth()
+
+  const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder
+
 
   return (
     <Container>
       <Profile to="/profile">
         <img 
-          src="https://github.com/BernardoSa01.png"
-          alt="Imagem de Bernardo Sá"
+          src={avatarUrl}
+          alt={user.name}
         />
 
         <div>
           <span>Bem-vindo,</span>
-          <strong>Bernardo Sá</strong>
+          <strong>{user.name}</strong>
         </div>
       </Profile>
 
--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -41,6 +41,33 @@ function AuthProvider({ children }) {
 
   }
 
+  // Atualizando perfil do usuário
+  async function updateProfile({ user, avatarFile }) {
+    try {
+
+      if(avatarFile) {
+        const fileUploadForm = new FormData()
+        fileUploadForm.append('avatar', avatarFile)
+
+        const response = await api.patch('/users/avatar', fileUploadForm)
+        user.avatar = response.data.avatar
+      }
+
+      await api.put('/users', user)
+      localStorage.setItem('@rocketnotes:user', JSON.stringify(user))
+
+      setData({ user, token: data.token })
+      alert('Perfil atualizado!')
+
+    } catch(error) {
+      if(error.response) {
+        alert(error.response.data.message)
+      } else {
+        alert('Não foi possível atualizar o perfil.')
+      }
+    }
+  }
+
   useEffect(() => {
     const token = localStorage.getItem('@rocketnotes:token')
     const user = localStorage.getItem('@rocketnotes:user')
@@ -59,6 +86,7 @@ function AuthProvider({ children }) {
     <AuthContext.Provider value={{ 
       SignIn, 
       signOut,
+      updateProfile,
       user: data.user
     }}
     >
